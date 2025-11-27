e62f5f867c2a5e3e5552800605691e255c9f212e
dudiddump <ith.aaa2611@gmail.com>
2025-11-27 20:26:52 +0700

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-// vite.config.ts
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/TalithaKhansa',
-  
   plugins: [react()],
-});
+  base: "/TalithaKhansa/", 
+})
